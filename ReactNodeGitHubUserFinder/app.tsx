b7af0c704bdafb8fb071aad63685b6cf139ce160
Andrea Manth <andrea@manth.se>
2018-11-15 15:31:39 +0100

    New Project - GitHubUsers

---
+++ ReactNodeGitHubUserFinder/app.tsx
@@ -0,0 +1,35 @@
+﻿declare var require: any
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const Card = (props) => {
+    
+        return (
+            <div>
+                <img src="http://placehold.it/75" />
+                <div>
+                    <div>Name</div>
+                    <div>Company</div>
+                </div>
+            </div>
+        );
+    }
+
+
+
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+              <Card/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Card />,
+    document.getElementById('root')
+);
+
