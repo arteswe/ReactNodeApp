b8492de70a6b8f50b75df43ab8c52c5e1a48b2fa
Andrea Manth <andrea@manth.se>
2018-11-15 10:00:33 +0100

    Complete game

--- ReactNodeApp/dist/app-bundle.js
+++ ReactNodeApp/dist/app-bundle.js
@@ -110,6 +110,9 @@ class Board extends React.Component {
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -120,7 +123,14 @@ class Board extends React.Component {
         return (React.createElement(Square, Object.assign({}, this.props, { value: this.state.squares[i], onClick: () => this.handleClick(i) })));
     }
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (React.createElement("div", null,
             React.createElement("div", { className: "status" }, status),
             React.createElement("div", { className: "board-row" },
@@ -136,6 +146,25 @@ class Board extends React.Component {
                 this.renderSquare(7),
                 this.renderSquare(8))));
     }
+}
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 class Game extends React.Component {
     render() {
